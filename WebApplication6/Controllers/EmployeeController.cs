75b6eebeaf5c3314cde4429282716811991b2783
Admin <kloklokesh846@gmail.com>
2021-12-31 14:24:42 +0530

    Add project files.

---
+++ WebApplication6/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { ID = 414, FirstName = "Bhavya", LastName = "Sree"});
+            employees.Add(new Employee() { ID = 415, FirstName = "Jaya", LastName = "Sree"});
+            employees.Add(new Employee() { ID = 416, FirstName = "Vali", LastName = "s"});
+
+            ViewData["employees"] = employees;
+
+            return View();
+        }
+    }
+}
